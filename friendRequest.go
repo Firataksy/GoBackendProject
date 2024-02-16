0e8d8af7e4a748600ef389a792bf45645bffcd71
1Firat <firat.aksoy@masomo.com>
2024-02-16 16:28:34 +0300

    fix: friendrequestcontrol added

--- friendRequest.go
+++ friendRequest.go
@@ -29,6 +29,23 @@ func friendRequest(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	friendRequestControl, _ := rc.ZRange(context.Background(), "friendrequest_"+IDUrl, 0, -1).Result()
+	friendRequestControl2, _ := rc.ZRange(context.Background(), "friendrequest_"+headerUserID, 0, -1).Result()
+
+	for _, data := range friendRequestControl {
+		if data == headerUserID {
+			responseFail(w, "you already sent a friend request")
+			return
+		}
+	}
+
+	for _, data := range friendRequestControl2 {
+		if data == headerUserID {
+			responseFail(w, "you already sent a friend request")
+			return
+		}
+	}
+
 	if IDUrl == headerUserID {
 		responseFail(w, "You cannot send yourself a friend request.")
 		return
