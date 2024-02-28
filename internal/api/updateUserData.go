dd1b13c5c11f58c244b6447d684513ff17a53a70
1Firat <firat.aksoy@masomo.com>
2024-02-28 15:12:56 +0300

    update user data rename and gitignore updated

--- internal/api/updateUserData.go
+++ internal/api/updateUserData.go
@@ -12,10 +12,10 @@ import (
 func (rc *RedisClient) UpdateUserData(w http.ResponseWriter, r *http.Request) {
 	var userData *Sign
 	var updatedUser UpdatedUser
-	var updateNewUserData UpdateNewUserData
+	var updatedNewUserData UpdateNewUserData
 
 	headerUserID := r.Header.Get("userid")
-	er := json.NewDecoder(r.Body).Decode(&updateNewUserData)
+	er := json.NewDecoder(r.Body).Decode(&updatedNewUserData)
 	if er != nil {
 		http.Error(w, er.Error(), http.StatusBadRequest)
 		return
@@ -32,42 +32,42 @@ func (rc *RedisClient) UpdateUserData(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal([]byte(checkUser), &userData)
 	json.Unmarshal([]byte(checkUser), &updatedUser)
 
-	if updateNewUserData.UserName == userData.UserName {
+	if updatedNewUserData.UserName == userData.UserName {
 		ResponseFail(w, "You already use this username")
 		return
 	}
 
-	checkUserID, _ := rc.Client.Get(context.Background(), updateNewUserData.UserName).Result()
+	checkUserID, _ := rc.Client.Get(context.Background(), updatedNewUserData.UserName).Result()
 
 	if checkUserID != "" {
 		ResponseFail(w, "Username is used")
 		return
 	}
 
-	if updateNewUserData.UserName != "" {
-		_, userRenameErr := rc.Client.Rename(context.Background(), userData.UserName, updateNewUserData.UserName).Result()
+	if updatedNewUserData.UserName != "" {
+		_, userRenameErr := rc.Client.Rename(context.Background(), userData.UserName, updatedNewUserData.UserName).Result()
 		if userRenameErr != nil {
 			log.Fatal("Redis Not Renamed Username", userRenameErr)
 		}
 		rc.Client.Del(context.Background(), "userID:"+userData.UserName)
-		userData.UserName = updateNewUserData.UserName
-		updatedUser.UserName = updateNewUserData.UserName
+		userData.UserName = updatedNewUserData.UserName
+		updatedUser.UserName = updatedNewUserData.UserName
 
 	}
 
-	if updateNewUserData.Password != "" {
-		hashPWD := utils.Md5Encode(updateNewUserData.Password)
+	if updatedNewUserData.Password != "" {
+		hashPWD := utils.Md5Encode(updatedNewUserData.Password)
 		userData.Password = hashPWD
 	}
 
-	if updateNewUserData.Name != "" {
-		userData.Name = updateNewUserData.Name
-		updatedUser.Name = updateNewUserData.Name
+	if updatedNewUserData.Name != "" {
+		userData.Name = updatedNewUserData.Name
+		updatedUser.Name = updatedNewUserData.Name
 	}
 
-	if updateNewUserData.SurName != "" {
-		userData.SurName = updateNewUserData.SurName
-		updatedUser.SurName = updateNewUserData.SurName
+	if updatedNewUserData.SurName != "" {
+		userData.SurName = updatedNewUserData.SurName
+		updatedUser.SurName = updatedNewUserData.SurName
 	}
 
 	rc.redisSetDataAndID(userData)
